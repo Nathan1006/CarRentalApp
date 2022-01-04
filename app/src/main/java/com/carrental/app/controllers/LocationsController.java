b84ba6f14357bca4988621ecfdf100051911e8ef
Nathan1006 <nrpetit@outlook.com>
2022-01-03 20:43:23 -0500

    Fixed Locations controller

--- app/src/main/java/com/carrental/app/controllers/LocationsController.java
+++ app/src/main/java/com/carrental/app/controllers/LocationsController.java
@@ -2,6 +2,7 @@ package com.carrental.app.controllers;
 
 import com.carrental.app.models.Location;
 import com.carrental.app.repositories.CustomerRepository;
+import com.carrental.app.repositories.LocationsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,35 +15,35 @@ import java.util.List;
 @RequestMapping("/api/locations")
 public class LocationsController {
     @Autowired
-    private LocationsController locations;
+    private LocationsRepository locations;
 
-//    @GetMapping
-//    public @ResponseBody List<Location> getAllVehicles() {
-//        return locations.findAll();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public @ResponseBody Location findLocation (@PathVariable Long id){
-//        return locations.findLocation(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<Location> createLocation (@RequestBody Location newLocation) {
-//        return new ResponseEntity<>(locations.save(newLocation), HttpStatus.CREATED);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public @ResponseBody Location updateLocation (@PathVariable Long id, @RequestBody Location updates) {
-//        Location location = locations.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-//
-//        if (updates.getAvailable() != null) location.setAvailable(updates.getAvailable());
-//
-//        return locations.save(updates);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<String> deleteLocation (@PathVariable Long id) {
-//        locations.deleteById(id);
-//        return new ResponseEntity<>("Deleted", HttpStatus.OK);
-//    }
+    @GetMapping
+    public @ResponseBody List<Location> getAllVehicles() {
+        return locations.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody Location findLocation (@PathVariable Long id){
+        return locations.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Location> createLocation (@RequestBody Location newLocation) {
+        return new ResponseEntity<>(locations.save(newLocation), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public @ResponseBody Location updateLocation (@PathVariable Long id, @RequestBody Location updates) {
+        Location location = locations.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (updates.getId() != null) location.setId(updates.getId());
+
+        return locations.save(updates);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteLocation (@PathVariable Long id) {
+        locations.deleteById(id);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
+    }
 }
